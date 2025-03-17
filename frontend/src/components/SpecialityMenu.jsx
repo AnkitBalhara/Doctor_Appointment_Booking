0d76530d05cad2bc639ebfcade56001ff735f45f
Ankit Balhara <ankitbalhara012@gmail.com>
2025-03-17 12:54:50 +0530

    Added Speciality Menu

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { specialityData } from "../assets/assets_frontend/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div id="speciality">
-      <h1>Find by Speciality </h1>
-      <p>
-        Simply browse through our extensive list of trusted doctors, schedule
+    <div id="speciality" className="flex flex-col items-center gap-4 py-16 text-gray-800 ">
+      <h1 className="font-medium text-3xl">Find by Speciality </h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors, schedule <br />
         your appointment hassle-free.
       </p>
 
-
-      <div>
-        {specialityData.map((item,index)=>{
-
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-hidden">
+        {specialityData.map((item, index) => {
+          return (
+            <Link key={index} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] duration-500 " to={`/doctors/${item.speciality}`}>
+              <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
+              <p>{item.speciality}</p>
+            </Link>
+          );
         })}
       </div>
     </div>
